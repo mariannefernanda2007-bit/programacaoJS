15cd083ad754176fb182c284296dda9d7b7b4c44
mariannefernanda2007-bit <mariannefernanda2007@gmail.com>
2026-08-17 17:23:57 -0300

    Listas de atividade_03

---
+++ 03_lista_atividades.js
@@ -0,0 +1,197 @@
+/*
+### ----- Atividade 0:
+
+Desenhe no terminal um retângulo usando apenas o comando do console.log
+ _____
+|     |
+|     |
+|     |
+|_____|
+*/
+
+let retangulo1 = " ______ "
+let retangulo2 ="|      |"
+let retangulo3 ="|      |"
+let retangulo4 ="|      |"
+let retangulo5 ="|______|"
+
+console.log(retangulo1)
+console.log(retangulo2)
+console.log(retangulo3)
+console.log(retangulo4)
+console.log(retangulo5)
+
+/*
+### ----- Atividade 1:
+
+Crie uma variável chamada `cidade` e outra chamada `estado`.
+Mostre na tela a frase: "Eu moro em X - Y"
+
+Substitua X pelo valor da variável `cidade` e Y pelo valor da variável `estado`.
+
+*/
+
+let cidade = "são carlos"
+let estado = "são paulo"
+
+console.log(" Eu moro em " + cidade + " " + estado)
+
+
+/*
+### ----- Atividade 2:
+
+Crie duas variáveis chamadas `numero1` e `numero2`.
+Atribua valores numéricos a elas.
+
+Crie uma variável chamada `soma` para armazenar o resultado da soma dos dois números.
+
+Mostre o resultado da soma na tela.
+
+*/
+
+let numero1 = 100
+let numero2 = 12
+let soma = numero1 + numero2
+
+console.log("A soma de"+ " " + numero1 + " " + "com" + " " + numero2  +  " "  + "é:" + soma)
+
+
+
+/*
+### ----- Atividade 3:
+
+Crie uma variável chamada `idade`.
+Mostre na tela a mensagem: "Olá, você tem X anos e daqui a 10 anos terá Y."
+
+Substitua X pelo valor da idade atual e Y pela idade que a pessoa terá daqui a 10 anos.
+
+*/
+let idade1 = 25
+let idade2 = 35
+let soma_idade = idade1 + idade2
+
+console.log("Olá, voce tem " + idade1 + " anos " + "daqui 10 anos terá" + soma_idade)
+
+/*
+### ----- Atividade 4:
+
+Crie uma variável chamada `numero`.
+
+Calcule o dobro e o triplo desse número.
+Armazene os resultados em duas novas variáveis.
+
+Mostre na tela o número original, seu dobro e seu triplo.
+
+*/
+let numero = 19
+let dobro = numero * 2
+let triplo = numero * 3
+
+console.log(" o dobro " + dobro + "o triplo "  + triplo)
+/*
+### ----- Atividade 5:
+
+Crie variáveis para armazenar o nome e o preço de dois produtos diferentes.
+
+Mostre na tela o nome e o preço de cada produto individualmente.
+Em seguida, calcule e mostre o valor total dos dois produtos.
+
+*/
+let preduto = "camiseta"
+let preco = 45.90
+
+let produto2 = "bolsa" 
+let preco2 = 38.50 
+
+console.log()
+
+
+/*
+### ----- Atividade 6:
+
+Crie uma variável chamada `salario`.
+
+Considere que a pessoa receberá um aumento de R$ 500 e um desconto do INSS de R$ 250
+Calcule o novo salário e armazene o resultado em uma nova variável.
+
+Mostre na tela o salário atual e o novo salário.
+
+*/
+let salario = 3000
+let
+/*
+### ----- Atividade 7:
+
+João recebe R$ 100 de mesada e gastou R$ 35.
+
+Crie variáveis para representar o valor da mesada e o valor gasto.
+Calcule quanto dinheiro sobrou e armazene o resultado em uma nova variável.
+
+Mostre na tela o valor da mesada, o valor gasto e quanto dinheiro sobrou.
+
+*/
+
+/*
+### ----- Atividade 8:
+
+Crie duas variáveis chamadas `base` e `altura`.
+
+Calcule a área de um retângulo utilizando esses dois valores.
+Armazene o resultado em uma nova variável.
+
+Mostre na tela o valor da base, da altura e da área calculada.
+
+*/
+
+/*
+### ----- Atividade 9:
+
+Crie uma variável chamada `celsius` para armazenar uma temperatura em graus Celsius.
+Converta essa temperatura para Fahrenheit e para Kelvin e mostre na tela os valores da conversão
+
+*/
+
+/*
+### ----- Atividade 10:
+
+Crie uma variável contendo uma quantidade de horas.
+
+Calcule quantos minutos correspondem a essa quantidade de horas.
+Calcule também quantos segundos correspondem a essa quantidade de horas.
+
+Mostre na tela a quantidade de horas, de minutos e de segundos.
+
+Exemplo:
+
+3 horas correspondem a 180 minutos ou 10800 segundos.
+
+*/
+
+
+/*
+### ----- Atividade 11:
+
+Crie duas variáveis de qualquer tipo.
+
+Em seguida, faça com que o valor da primeira variável seja armazenado na segunda variável,
+e que o valor original da segunda variável seja armazenado na primeira variável.
+
+Ao final, mostre o valor das duas variáveis na tela.
+
+*/
+
+/*
+### ----- Atividade 12:
+
+Crie variáveis para armazenar a distância percorrida por um carro,
+a quantidade de litros de combustível utilizada e o preço do litro do combustível.
+
+Calcule quantos quilômetros o carro percorreu para cada litro de combustível.
+
+Calcule também quanto foi gasto com combustível durante a viagem.
+
+Mostre na tela a distância percorrida, a quantidade de litros utilizados,
+o consumo do carro e o valor total gasto com combustível.
+
+*/
+
