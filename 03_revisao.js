f7d806eba5f27430fe87996b3a659db60f951ef5
mariannefernanda2007-bit <mariannefernanda2007@gmail.com>
2026-08-17 14:48:07 -0300

    revisao e acumulo de variaveis

---
+++ 03_revisao.js
@@ -0,0 +1,36 @@
+/*
+let aluguel = 1200.00
+let condominio = 250.59
+let internet = 85.99
+let total = aluguel + condominio + internet
+
+console.log ("O total das contas é: " + total)
+*/
+
+
+//--------------------------------------------------------------------------------------------------------
+
+let nome = "marianne"
+let sobrenome = "pereira"
+let nome_completo = nome+ " " +sobrenome
+
+console.log("Seja bem-vindo(a)" +nome_completo)
+
+
+//--------------------------------------------------------------------------------------------------------
+
+let salario = 2000
+console.log("Seu salario é R$ " +salario)
+
+let comissao = 100
+salario = salario + comissao // acumulo: adicionar um novo valor na variavel
+console.log("Salario com comissao: " +salario)
+
+let adicional_noturno =200
+salario = salario + adicional_noturno
+console.log("salario com adicional noturno: " +salario)
+
+/*
+isso é um comentário
+de multiplas linhas
+*/
